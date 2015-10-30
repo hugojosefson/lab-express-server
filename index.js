fde2a3fb3cd2573e43e3da66898795c2ac88ae69
Hugo Josefson <hugo@josefson.org>
2015-10-30 13:29:13 +0100

    nodemon

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import express from 'express';
 import _ from 'lodash';
 const app = express();
 
-const log = (req,res) => res.send(JSON.stringify(_.pick(req, ['method', 'url','originalUrl']), null, 2));
+const log = (req, res) => res.send(JSON.stringify(_.pick(req, ['method', 'url', 'originalUrl']), null, 2));
 
-app.use(log);
+app.route('*')
+    .all(log);
 
-app.listen(3001);
+const port = process.env.port || 3001;
+app.listen(port, err => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Listening on port ' + port);
+    }
+});
