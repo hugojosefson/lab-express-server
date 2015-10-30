95579e3ae7fb3bd8b65606e874d9ba33703e7ae6
Hugo Josefson <hugo@josefson.org>
2015-10-30 13:07:17 +0100

    babel5

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import express from 'express';
 import _ from 'lodash';
 const app = express();
 
-const log = (req,res) => {
-    res.send(JSON.stringify(_.pick(req, ['method', 'url','originalUrl']), null, 2));
-};
+const log = (req,res) => res.send(JSON.stringify(_.pick(req, ['method', 'url','originalUrl']), null, 2));
 
 app.use(log);
 
