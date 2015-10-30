6f5360b61262b884b0ad9eeb848ace299058a7e9
Hugo Josefson <hugo@josefson.org>
2015-10-30 13:44:14 +0100

    log instead of echo

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import auth from 'basic-auth';
 import _ from 'lodash';
 const app = express();
 
-const log = (req, res) => res.send(
-    JSON.stringify(
-        _.assign(_.pick(req, ['method', 'url', 'originalUrl']), {auth: auth(req)}),
-        null, 2
-    )
-);
+const log = (req, res, next) => {
+    console.log(
+        JSON.stringify(
+            _.assign(_.pick(req, ['method', 'url', 'originalUrl']), {auth: auth(req)}),
+            null, 2
+        )
+    );
+    next();
+};
 
 app.route('*')
     .all(log);
