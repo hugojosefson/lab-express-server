d1fb820236efb68197396502d65c2b4aaed8cf69
Hugo Josefson <hugo@josefson.org>
2015-12-05 18:26:24 +0100

    Log more and respond 204.

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from 'express';
 import auth from 'basic-auth';
 import _ from 'lodash';
+import bodyParser from 'body-parser';
+
 const app = express();
 
 const log = (req, res, next) => {
     console.log(
         JSON.stringify(
-            _.assign(_.pick(req, ['method', 'url', 'originalUrl']), {auth: auth(req)}),
+            _.assign(_.pick(req, ['method', 'url', 'originalUrl', 'query', 'body', 'headers']), {auth: auth(req)}),
             null, 2
         )
     );
     next();
 };
 
+const respond204 = (req, res) => res.sendStatus(204);
+
+app.use(bodyParser.json());
+
 app.route('*')
-    .all(log);
+    .all(log)
+    .all(respond204);
 
 const port = process.env.port || 3001;
 app.listen(port, err => {
