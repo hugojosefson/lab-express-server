f19e647af345e888e758c96d0bc0a5a9ce194230
Hugo Josefson <hugo@josefson.org>
2019-07-13 21:51:08 +0000

    Upgrade to node@12.

--- src/server.js
+++ src/server.js
@@ -1,33 +1,33 @@
-import express from 'express';
-import auth from 'basic-auth';
-import _ from 'lodash';
-import bodyParser from 'body-parser';
+import express from 'express'
+import auth from 'basic-auth'
+import bodyParser from 'body-parser'
 
-const app = express();
+const app = express()
 
 const log = (req, res, next) => {
-    console.log(
-        JSON.stringify(
-            _.assign(_.pick(req, ['method', 'url', 'originalUrl', 'query', 'body', 'headers']), {auth: auth(req)}),
-            null, 2
-        )
-    );
-    next();
-};
+  const { method, url, originalUrl, query, body, headers } = req
+  console.log(
+    JSON.stringify(
+      { method, url, originalUrl, query, body, headers, auth: auth(req) },
+      null, 2
+    )
+  )
+  next()
+}
 
-const respond204 = (req, res) => res.sendStatus(204);
+const respond204 = (req, res) => res.sendStatus(204)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.route('*')
-    .all(log)
-    .all(respond204);
+  .all(log)
+  .all(respond204)
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 app.listen(port, err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Listening on port ' + port);
-    }
-});
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('Listening on port ' + port)
+  }
+})
